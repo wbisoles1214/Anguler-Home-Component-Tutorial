f7e8de6c37b1e98ad684026891789e0880727c80
wbisoles1214 <wbisoles@gmail.com>
2024-02-24 18:11:34 +0800

    Your commit message here

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,12 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'], // Use styleUrls instead of styleUrl
 })
 export class HomeComponent {
-
+  title = 'homes';
 }
